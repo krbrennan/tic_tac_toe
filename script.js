b0cda23184a6b4bc41a20c91daf2eaa22c14704d
krbrennan <krb.brennan@gmail.com>
2018-05-09 17:01:54 -0400

    Fully functional 2player. Reset Button, winningEffects, game logic all operational

--- script.js
+++ script.js
@@ -41,18 +41,23 @@ function handleMove(box) {
   // else change current player
   currentPlayerPositions();
 
-  if (isWinner()) {
-    showResetBtn();
-    colorBoxes();
-    return announceWinner();
-  } else {
-    if (currentPlayer === 'X') {
-      currentPlayer = 'O';
-    } else currentPlayer = 'X';
-  };
+  // I'm not sure why isWinner() is needed but it is...
+  // if (isWinner()) {
+  // }
+  isWinner();
+  switchPlayer();
+
+}
+
+
+function switchPlayer() {
+  if (currentPlayer === 'X') {
+    currentPlayer = 'O';
+  } else currentPlayer = 'X';
+}
+
   // next, check to see if the move won.
   // probably want to move what's below into that check.
-}
 
 
 function winningCombo(ele) {
@@ -61,43 +66,25 @@ function winningCombo(ele) {
 }
 
 function isWinner() {
-  const diagonal = [[0,4,8],[2,4,6]];
-  const across = [[0,1,2],[3,4,5],[6,7,8]];
-  const vertical = [[0,3,6],[1,4,7],[2,5,8]];
+  const winningCombos = [[0,4,8],[2,4,6],[0,1,2],[3,4,5],[6,7,8],
+                        [0,3,6],[1,4,7],[2,5,8]];
 
   let thereIsAWinner = false;
-
   let currentPlayerMoves = currentPlayerPositions();
-
-  let winnerBoxesToColorIn = [];
-  // console.log(currentPlayerMoves);
 
   // aggregate all currentPlayer's positions__^
   // run through diagonal,across,vertical
   // if all of a set is contained (.every(winningCombo)) return true,
   // announce winner
 
-  diagonal.forEach(function(set) {
+  winningCombos.forEach(function(set) {
     if (contains(currentPlayerMoves, set)) {
       thereIsAWinner = true;
-      return thereIsAWinner;
+      colorBoxes(set);
+      thereIsAWinner;
     }
   });
-
-  across.forEach(function(set) {
-    if (contains(currentPlayerMoves, set)) {
-      thereIsAWinner = true;
-      return thereIsAWinner;
-    }
-  });
-
-  vertical.forEach(function(set) {
-    if (contains(currentPlayerMoves, set)) {
-      thereIsAWinner = true;
-      return thereIsAWinner;
-    }
-  });
-  return thereIsAWinner;
+  thereIsAWinner;
 }
 
 // this function takes in currentPlayer moves array and checks
@@ -128,6 +115,7 @@ function announceWinner() {
   winner.innerHTML = `${currentPlayer} Wins!`
   winner.style.visibility='visible';
   currentPlayer = 'X';
+  colorBoxes;
   // add playAgain button
 }
 
@@ -141,11 +129,30 @@ resetBtn.addEventListener('click', stopPlaying);
 function stopPlaying() {
   boxes.forEach(function(box) {
     box.innerHTML = "";
+    box.style.color='black';
+    box.style.background='white';
     winner.style.visibility='hidden';
     resetBtn.style.visibility='hidden';
   });
 }
 
-function colorBoxes() {
-
+function colorBoxes(winners) {
+  let winningNums = winners;
+  // console.log(winningNums);
+  boxes.forEach(function(box) {
+    let id = parseInt(box.id);
+    if (id === winningNums[0]) {
+      box.style.color="red";
+      box.style.background='yellow';
+    } else if (id === winningNums[1]) {
+      box.style.color="red";
+      box.style.background='yellow';
+    } else if (id === winningNums[2]) {
+      box.style.color="red";
+      box.style.background='yellow';
+    }
+  });
+  announceWinner();
+  showResetBtn();
+  switchPlayer();
 }
